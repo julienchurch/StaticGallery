8c5cbad0d2fa171ea033ac803d3365053940e7f7
Julien Church <julienchurch@gmail.com>
2016-02-07 20:52:13 -0500

    Break out reset styles into task

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,21 @@ var gulp   = require('gulp'),
     sass   = require('gulp-sass'),
     rename = require('gulp-rename');
 
-gulp.task('compile_scss', function() {
+gulp.task('compile-StaticGallery', function() {
   gulp.src('src/Main.scss')
       .pipe(sass().on('error', sass.logError))
       .pipe(rename('static_gallery.css'))
       .pipe(gulp.dest('./example'));
 });
 
+gulp.task('compile-prettyAndUseless', function() {
+  gulp.src('src/lib/prettyAndUseless.scss')
+      .pipe(sass().on('error', sass.logError))
+      .pipe(rename('pretty_and_useless.css'))
+      .pipe(gulp.dest('./example'));
+})
+
 gulp.task('default', function() {
-  gulp.watch('src/**/*.scss', ['compile_scss']);
+  gulp.watch('src/**/*.scss', [ 'compile-StaticGallery'
+                              , 'compile-prettyAndUseless' ]);
 });
